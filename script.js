d3125d6277e76506eb2778c2fd92fcbf69a84533
tororon8 <sato_yuto@thruster.co.jp>
2025-02-18 18:24:19 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('yesButton').addEventListener('click', () => {
+    alert('「はい」を選びました！');
+});
+
+document.getElementById('noButton').addEventListener('click', () => {
+    alert('「いいえ」を選びました！');
+});
